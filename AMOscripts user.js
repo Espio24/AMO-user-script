96d5f2a984db038158503b5076520f47a759bec6
Espio24 <63601588+Espio24@users.noreply.github.com>
2022-10-25 16:12:06 +0300

    Update AMOscripts user.js

--- AMOscripts user.js
+++ AMOscripts user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name AMOscripts
 // @namespace http://tampermonkey.net/
-// @version 0.1
+// @version 1.1
 // @description try to take over the world!
 // @author You
 // @match https://sshagin.amocrm.ru/leads/detail*
